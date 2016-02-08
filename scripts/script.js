1ac065d91bb45350b050e71aaa14ba81a8c40f11
Gordon Connell <gordon@emagid.com>
2016-02-08 16:16:46 -0500

    custom message

--- scripts/script.js
+++ scripts/script.js
@@ -7,34 +7,41 @@ $( document ).ready(function() {
   } else if (window.location.pathname == "/gmoney/" + tabs[2]) {
     $(".work_tab").addClass("active");
   } else if (window.location.pathname == "/gmoney/" + tabs[3]) {
-    $('body').css({'background': '#000'});
     $(".hobbies_tab").addClass("active");
   } else if (window.location.pathname == "/gmoney/" + tabs[4]) {
     $(".about_tab").addClass("active");
   };
 
 	$(".smash_slide").hover(function(){
-       $(".smash_slide img").show();
-       $(".smash_slide img").animate({
+       $(".smash_icon").show();
+       $(".smash_icon").animate({
        	'margin-left': '100%'
-       }); 
+       });
+       $(".left_red_arrow").show();
+       $(".left_arrow").hide();
     }, function(){
-       $(".smash_slide img").animate({
+       $(".smash_icon").animate({
        	'margin-left': '0'
        });
+       $(".left_red_arrow").hide();
+       $(".left_arrow").show();
 
-       $(".smash_slide img").hide(500);
+       $(".smash_icon").hide(500);
     });
 
     $(".skate_slide").hover(function(){
-       $(".skate_slide img").show();
-       $(".skate_slide img").animate({
+       $(".skate_icon").show();
+       $(".skate_icon").animate({
        	'margin-left': '-100%'
        });
+       $(".right_arrow").hide();      
+       $(".right_red_arrow").show();      
     }, function(){
-    	$(".skate_slide img").animate({
+    	$(".skate_icon").animate({
     		'margin-left': '0'
     	});
-    	$(".skate_slide img").hide(500);
+      $(".right_arrow").show();       
+       $(".right_red_arrow").hide();      
+    	$(".skate_icon").hide(500);
     });
 });
