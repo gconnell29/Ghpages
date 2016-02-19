7ea340cb34a78e8212b9eb3ecaf06711e4e05c27
gconnell29 <gconnelljr29@gmail.com>
2016-02-19 01:13:14 -0500

    copying ghpages to master

--- push.sh
+++
@@ -1,5 +0,0 @@
-git add -A
-
-git commit -m "custom message"
-
-git push origin gh-pages
--- scripts/script.js
+++ scripts/script.js
@@ -1,92 +1,135 @@
-$( document ).ready(function() {
-	$(".phrases").hide();
-
-	$( ".profile-pic" ).click(function() {
-		$(".phrases").toggle();
-		$(".name").css(["color: red;"]);
-	});
-
-	$( ".form_tab" ).click(function() {
-		$(this).next().toggle();
-	});
-
-	$( ".showmore_distant" ).click(function() {
-		$(".more_distant").show();
-		$(".showless_distant").show();
-		$(this).hide();
-	});
-
-	$( ".showless_distant" ).click(function() {
-		$(".more_distant").hide();
-		$(".showmore_distant").show();
-		$(this).hide();
-	});
-
-	$(window).scroll(function() {
-		if ($(window).scrollTop() >= 350) {
-			$('.aside').css({
-				'position': 'fixed',
-				'top': '20px', 
-				'width': '150px' 
-			});
-		};
-
-		if ($(window).scrollTop() <= 349) {
-			$('.aside').css({
-				'position': 'initial',
-				'top': 'initial' 
-			});
-		};
-
-		console.log($(window).scrollTop());
-	});
+// remove preload to allow css transitinos to fire
+$( window ).load(function() {
+  /*alert("removed preload class");
+  $('#work').removeClass("preload");*/
 });
 
+$( document ).ready(function() {
+  $('#work').removeClass("preload");
+  // alert("message 1");
 
-function validateSignUp() {
-	if(validatePassword() == false){
-        return false;
-    }
-    alert("yo");
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var fname = document.getElementById("fname").value;
+  // nav tab color
+  var tabs = ["index.html", "people.html", "work.html", "hobbies.html", "about.html"];
+
+  if (window.location.pathname == "/gordon/" + tabs[1]) {
+    $(".people_tab").addClass("active");
+  } else if (window.location.pathname == "/gordon/" + tabs[2]) {
+    $(".work_tab").addClass("active");
+  } else if (window.location.pathname == "/gordon/" + tabs[3]) {
+    $(".hobbies_tab").addClass("active");
+  } else if (window.location.pathname == "/gordon/" + tabs[4]) {
+    $(".about_tab").addClass("active");
+  } else if (window.location.pathname == "/gordon/" + tabs[4] + "#smash") {
+    $(".about_tab").addClass("active");
+  } else if (window.location.pathname == "/gordon/" + tabs[4] + "#skate") {
+    $(".about_tab").addClass("active");
+  };
 
 
-    if (typeof(Storage) != "undefined") {
-        sessionStorage.setItem("username", username);
-        sessionStorage.setItem("password", password);
-        sessionStorage.setItem("firstName", firstName);
+  // mobile nav slider
+  // $('#simple-menu').sidr();
 
-        alert("REGISTER SUCCESSFUL");
-        setErrorMessage("signUpErrorMessage","Successfully Registered");
-    	document.getElementById('signUpErrorMessage').innerHTML= "SignUp not Successful";
-        return true;
-    }
+  $('#simple-menu').sidr({
+    name: 'sidr-right',
+    side: 'right'
+  });
 
-    alert("REGISTER UNSUCCESSFUL");
-    document.getElementById('signUpErrorMessage').innerHTML= "SignUp not Successful";
-    return false;
-}
+  /*$('#existing-content-menu').sidr({
+    name: 'sidr-existing-content',
+    source: '#demoheader, #demo-content'
+  });
+*/
 
-function validPass(){
-    var fname = document.getElementById("fname").value;
-    var lname = document.getElementById("lname").value;
-    var p = document.getElementById("password").value;
-    var vp = document.getElementById("vpassword").value;
-    var email = document.getElementById("email").value;
+	$(".smash_slide").hover(function(){
+       $(".smash_icon").show();
+       $(".smash_icon").animate({
+       	'margin-left': '100%'
+       });
+       $(".left_red_arrow").show();
+       $(".left_arrow").hide();
+    }, function(){
+       $(".smash_icon").animate({
+       	'margin-left': '0'
+       });
+       $(".left_red_arrow").hide();
+       $(".left_arrow").show();
 
-	if(p!=vp){
-		document.getElementById('errorMessage').innerHTML="Passwords do not match!";
-		return false;
-	 }
+       $(".smash_icon").hide(500);
+    });
 
-	if (typeof(Storage) != "undefined") {
-		sessionStorage.setItem("email", email);
-		sessionStorage.setItem("password", p);
-		sessionStorage.setItem("fname", fname);
-	}
+    $(".skate_slide").hover(function(){
+       $(".skate_icon").show();
+       $(".skate_icon").animate({
+       	'margin-left': '-100%'
+       });
+       $(".right_arrow").hide();      
+       $(".right_red_arrow").show();      
+    }, function(){
+    	$(".skate_icon").animate({
+    		'margin-left': '0'
+    	});
+      $(".right_arrow").show();       
+       $(".right_red_arrow").hide();      
+    	$(".skate_icon").hide(500);
+    });
 
-	document.getElementById('errorMessage').innerHTML="";
-		return true;
-}
+
+    //WORK PAGE
+
+    var ul = $("<ul></ul>");
+
+    var data = [
+      'Created sleek designs with HTML/CSS',
+      'Created responsive designs for mobile',
+      'Lead front end development for all projects',
+      'Tested functionality across all browsers',
+      'Managed and collaborated codebases via Git',
+      'Formatted weekly email newsletters in html'
+    ];
+
+    for (var i = 0; i < data.length; i++) {
+      var li_list = $("<li></li>").html(data[i]);
+      ul.append(li_list);
+    };
+
+    $("#duties").prepend(ul);
+
+
+    if ($(window).width() >= 1025) {
+      $('.work_content').hover(function(){
+        $('.project').fadeTo(100, 0.33);
+      }, function(){
+        $('.project').fadeTo(100, 1);
+      });
+    } else if ($(window).width() <= 1025) {
+      $('.work_content').hover(function(){
+        // $('.project').fadeTo(100, 0.33);
+      }, function(){
+        // $('.project').fadeTo(100, 1);
+      });
+    };
+
+
+    // ABOUT PAGE
+    // slider
+      $("#owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      stopOnHover: true,
+      pagination: false,
+      navigationText: ["<img src='images/owl-prev.png' />","<img src='images/owl-next.png' />"]
+      // autoPlay: 1000
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+    });
+
+});
+
